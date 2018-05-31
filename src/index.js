acd4e641e6995d4975894c194694f8153197ce5d
Roc Wang <rocwangi@sina.cn>
2018-05-31 17:07:11 +0800

    make content section can't click when overlay show

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import {render} from "react-dom";
 import Model from "./model";
 
 const div = document.createElement("div");
+div.style.position = 'fixed';
+div.style.left = 0;
+div.style.top = 0;
+div.style.width = '100%';
+div.style.height = '100%';
 document.body.appendChild(div);
 const container = render(<Model />, div);
 
